cf90a256a9b58629df0c5310ecc73db916c39349
Aur3l14no <muzhi.yu1@gmail.com>
2020-04-13 23:43:02 +0800

    hammperspoon

--- module-config.lua
+++ module-config.lua
@@ -4,6 +4,7 @@ key2App = {
     ['0'] = {'/Applications/WeChat.app', 'Chinese', 0},
     -- ['9'] = {'/Applications/QQ.app', 'Chinese', 0},
     ['8'] = {'/Applications/Telegram Desktop.app', 'Chinese', 0},
+    ['7'] = {'/Applications/TeamSpeak 3 Client.app', 'Chinese', 0},
     p = {'/System/Library/CoreServices/Finder.app', 'English', 0},
     o = {'/Applications/Emacs.app', 'English', 0},
     i = {'/Applications/iTerm.app', 'English', 0},
