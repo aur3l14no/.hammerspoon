daba189662f29f8088c0e63ce2154ffe901d2b49
Aur3l14no <muzhi.yu1@gmail.com>
2021-05-28 21:01:39 +0800

    hammerspoon: IMPROVE layout

--- module-layout.lua
+++ module-layout.lua
@@ -13,39 +13,41 @@ presetLayout = {
   ['corner']       = hs.geometry.unitrect(0.95   , 0.95 , 0.05 , 0.05),
 }
 
-presetLayouts = {
-  {"Chrome + Shell + Finder", "4", {
-     {'com.google.Chrome', nil, nil, presetLayout['left'], nil, nil},
-     {'com.googlecode.iterm2', nil, nil, presetLayout['right-top'], nil, nil},
-     {'com.apple.finder', nil, nil, presetLayout['right-bottom'], nil, nil},
+savedLayouts = {
+  ['1'] = {'User-1', {}, {}},
+  ['2'] = {'User-2', {}, {}},
+  ['3'] = {'PDF + Emacs', {
+             {'com.readdle.PDFExpert-Mac', nil, nil, presetLayout['left'], nil, nil},
+             {'org.gnu.Emacs', nil, nil, presetLayout['right'], nil, nil},
   }},
-  {"PDF + Emacs", "3", {
-     {'com.readdle.PDFExpert-Mac', nil, nil, presetLayout['left'], nil, nil},
-     {'org.gnu.Emacs', nil, nil, presetLayout['right'], nil, nil},
+  ['4'] = {'Chrome + Shell + Finder', {
+             {'com.google.Chrome', nil, nil, presetLayout['left'], nil, nil},
+             {'com.googlecode.iterm2', nil, nil, presetLayout['right-top'], nil, nil},
+             {'com.apple.finder', nil, nil, presetLayout['right-bottom'], nil, nil},
   }},
-  {"Music", "5", {
-     {'com.netease.163music', nil, nil, presetLayout['center-large'], nil, nil},
+  ['5'] = {'Music', {
+             {'com.netease.163music', nil, nil, presetLayout['center-large'], nil, nil},
   }},
 }
 
 -- Layout switching
 
-savedLayouts = {}
-
 function saveCurrentLayout(dest)
-  savedLayouts[dest] = {}
   local wins = hs.window.orderedWindows()
-  for i = 1, #wins do
-    local win = wins[i]
+  for _, win in ipairs(wins) do
     local frame = win:frame()
-    savedLayouts[dest][i] = {nil, win, nil, nil, frame, nil, options={['absolute_x'] = true, ['absolute_y'] = true}}
+    table.insert(savedLayouts[dest][2], {nil, win, nil, nil, frame, nil, options={['absolute_x'] = true, ['absolute_y'] = true}})
   end
-  -- printTable(savedLayouts)
+  local wins = hs.window.invisibleWindows()
+  for _, win in ipairs(wins) do
+    local frame = win:frame()
+    table.insert(savedLayouts[dest][3], {nil, win, nil, nil, frame, nil, options={['absolute_x'] = true, ['absolute_y'] = true}})
+  end
+  -- printTable(savedLayouts["1"])
 end
 
 
-currentLayout = "1"
-function freshApplyLayout(layout)
+function freshApplyLayout(name, visibleLayout, invisibleLayout)
   -- Hide windows, apply layout
   -- Only launch app when using preset layout (when appBundleID ~= nil)
   -- Requires layout[1] to be appBundleID or layout[2] to be hs.window
@@ -53,9 +55,9 @@ function freshApplyLayout(layout)
   local visibleWins = hs.window.visibleWindows()
 
   -- Launch or focus (back to front)
-  for i = 1, #layout do
-    local appBundleID = layout[#layout + 1 - i][1]
-    local win = layout[#layout + 1 - i][2]
+  for i = 1, #visibleLayout do
+    local appBundleID = visibleLayout[#visibleLayout + 1 - i][1]
+    local win = visibleLayout[#visibleLayout + 1 - i][2]
     if win ~= nil then
       win:focus()
     elseif appBundleID ~= nil then
@@ -63,16 +65,16 @@ function freshApplyLayout(layout)
     end
   end
 
-  -- Apply layout
-  hs.layout.apply(layout)
+  -- Apply visible layout
+  hs.layout.apply(visibleLayout)
 
   -- Hide all irrelavent windows
   for _, win in ipairs(visibleWins) do
     local tries = 0
     local app = win:application()
 
-    -- Check if the app is in layout apps
-    for _, item in ipairs(layout) do
+    -- Check if the app is in visibleLayout apps
+    for _, item in ipairs(visibleLayout) do
       if app:bundleID() == item[1] or win == item[2] then
         goto continue
       end
@@ -83,44 +85,40 @@ function freshApplyLayout(layout)
     end
     if not app:isHidden() then
       print('Failed to hide:')
-      printTable(win)
+      printTable(app)
     end
     ::continue::
   end
+
+  -- Apply invisible layout
+  if invisibleLayout ~= nil then
+    hs.layout.apply(invisibleLayout)
+  end
+
+  hs.alert.show(string.format("Layout applied: %s", name))
 end
 
+emptySlots = {["1"] = true, ["2"] = true}
 
-for _, preset in ipairs(presetLayouts) do
-  local name, key, layout = table.unpack(preset)
+currentLayout = "1"
+for key, layout in pairs(savedLayouts) do
+  local name, visibleLayout, invisibleLayout = table.unpack(layout)
   hs.hotkey.bind(
     hyper, key,
     function()
-      if currentLayout == "1" then
+      if emptySlots[currentLayout] then
         saveCurrentLayout(currentLayout)
       end
-      freshApplyLayout(layout)
+      freshApplyLayout(name, visibleLayout, invisibleLayout)
       currentLayout = key
-      hs.alert.show(string.format("Layout %s: %s", key, name))
     end
   )
-end
-
-for _, key in ipairs({"1", "2"}) do
-  savedLayouts[key] = {}
-  hs.hotkey.bind(
-    hyper, key,
-    function()
-      freshApplyLayout(savedLayouts[key])
-      currentLayout = key
-      hs.alert.show(string.format("Layout %s", key))
-      -- myAlert(string.format("Layout %s restored!", key))
-    end
-  )
-  hs.hotkey.bind(
-    super, key,
-    function()
-      saveCurrentLayout(key)
-      -- myAlert(string.format("Layout %s saved!", key))
-    end
-  )
+  if emptySlots[key] then
+    hs.hotkey.bind(
+      super, key,
+      function()
+        saveCurrentLayout(key)
+      end
+    )
+  end
 end
