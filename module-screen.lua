d856d315c41a282300b42b210ee352d91bd47b98
aur3l14no <muzhi.yu1@gmail.com>
2021-06-28 22:53:51 +0800

    hs: remember mouse when switching focus between screens

--- module-screen.lua
+++ module-screen.lua
@@ -44,18 +44,27 @@ function moveAllWindowsTo(screenName, showNotify)
   end
 end
 
+screenFocusMemory = {}
 function focusScreen(rel)
   local focusedWin = hs.window.focusedWindow()
   local screen = focusedWin:screen()
+  local pos = hs.mouse.getRelativePosition()
+  screenFocusMemory[screen:getUUID()] = {focusedWin, pos}
   if rel == 1 then
     screen = screen:next()
   elseif rel == -1 then
     screen = screen:previous()
   end
-  for _, win in ipairs(hs.window.filter.default:getWindows()) do
-    if win:screen() == screen then
-      win:focus()
-      break
+  local pack = screenFocusMemory[screen:getUUID()]
+  if pack and pack[1]:isVisible() then
+    pack[1]:focus()
+    hs.mouse.setRelativePosition(pack[2], screen)
+  else
+    for _, win in ipairs(hs.window.filter.default:getWindows()) do
+      if win:screen() == screen then
+        win:focus()
+        break
+      end
     end
   end
 end
