d17204663a2fbe824e76923541c22fe3f31a9e17
aur3l14no <muzhi.yu1@gmail.com>
2021-12-21 00:48:16 +0800

    hs: app config

--- module-config.lua
+++ module-config.lua
@@ -4,12 +4,12 @@ super = {'ctrl', 'command', 'option', 'shift'}
 key2App = {
   {'0', 'com.tencent.xinWeChat'},
   {'8', 'ru.keepcoder.Telegram'},
-  -- {'7', 'com.electron.yesplaymusic'},
   {'7', 'com.netease.163music'},
   {'o', 'org.gnu.Emacs'},
   {'p', 'com.apple.finder'},
   {'i', 'com.googlecode.iterm2'},
   {'k', 'com.google.Chrome'},
+  {'j', 'com.TickTick.task.mac'},
   {'b', 'com.microsoft.VSCode'},
   -- {'m', 'com.toketaware.ithoughtsx'},
   -- {'j', 'com.literatureandlatte.scapple'},
