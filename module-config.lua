bb2dd3464a33c22ac2a6b01e6b75a57ff3a79ebb
aur3l14no <muzhi.yu1@gmail.com>
2022-01-08 15:33:32 +0800

    hs: config

--- module-config.lua
+++ module-config.lua
@@ -3,17 +3,19 @@ super = {'ctrl', 'command', 'option', 'shift'}
 
 key2App = {
   {'0', 'com.tencent.xinWeChat'},
+  {'9', 'com.tencent.qq'},
   {'8', 'ru.keepcoder.Telegram'},
   {'7', 'com.netease.163music'},
-  {'o', 'org.gnu.Emacs'},
+  {'e', 'org.gnu.Emacs'},
+  {'o', 'com.electron.logseq'},
   {'p', 'com.apple.finder'},
   {'i', 'com.googlecode.iterm2'},
   {'k', 'com.google.Chrome'},
-  {'j', 'com.TickTick.task.mac'},
+  {'j', 'com.microsoft.edgemac'},
+  {'\'', 'com.TickTick.task.mac'},
   {'b', 'com.microsoft.VSCode'},
   -- {'m', 'com.toketaware.ithoughtsx'},
-  -- {'j', 'com.literatureandlatte.scapple'},
-  {'m', 'com.google.Chrome.app.Default-pebppomjfocnoigkeepgbmcifnnlndla'},
-  {'n', 'com.google.Chrome.app.kmcnjdcipmgchfndpapkoecbidofdpbc'},
+  {'m', 'com.google.Chrome.app.Default-pebppomjfocnoigkeepgbmcifnnlndla'},  -- diagrams.net
+  {'n', 'com.google.Chrome.app.kmcnjdcipmgchfndpapkoecbidofdpbc'},  -- excalidraw
 }
 
